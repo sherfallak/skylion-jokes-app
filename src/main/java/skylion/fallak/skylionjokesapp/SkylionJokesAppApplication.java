d024003f3542e484a4720b6ae0778d365e98b903
sherfallak <sh.fallak@gmail.com>
2022-05-20 00:09:40 +0200

    Created Spring Boot Project. closes #1.

---
+++ src/main/java/skylion/fallak/skylionjokesapp/SkylionJokesAppApplication.java
@@ -0,0 +1,13 @@
+package skylion.fallak.skylionjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylionJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkylionJokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skylion/fallak/skylionjokesapp/SkylionJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package skylion.fallak.skylionjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylionJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
