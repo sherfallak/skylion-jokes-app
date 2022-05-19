67ba71676630803b9ebbbdf783c35b657bf10620
sherfallak <sh.fallak@gmail.com>
2022-05-20 01:16:03 +0200

    add controller for service. closes #4.

---
+++ src/main/java/skylion/fallak/skylionjokesapp/controllers/JokeController.java
@@ -0,0 +1,27 @@
+package skylion.fallak.skylionjokesapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import skylion.fallak.skylionjokesapp.services.JokeService;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",
+            ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+
+
+    }
+}
